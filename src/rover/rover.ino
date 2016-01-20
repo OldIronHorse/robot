2dcfd7f85fc773cd0a2a7bb81f3b0f90dcecde18
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-01-20 15:30:35 +0000

    algo abstraction removed. Use Arduino setup(), loop() and interrupts instead.

--- src/rover/rover.ino
+++ src/rover/rover.ino
@@ -1,20 +1,31 @@
 #define DEBUG
 #include <DebugUtils.h>
 #include <rover.h>
-#include "algo.h"
+
+//TODO: Interrupt-based range checking?
+// algorithm is setup, loop and callback function set
+// setup() configures iterrupts and callback is required
+// This looks a lot like a main.ino file...
+// So maybe no abstraction is required, just work with the arduino structure.
 
 void setup(){
   DEBUG_INIT(9600)
   rover::setup();
 }
 
+const long min_range_cm = 30;
+const long go_range_cm = 100;
+
 void loop(){
-  //forward();
-  turn_right_to_avoid(30, 100);
-  /*switch(rover::command_state()){
-    case rover::cmd_go:
-      turn_right_to_avoid(20);
-    default:
-      rover::stop();
-  }*/
+  DEBUG_PRINT(rover::range())
+  if(rover::range() < min_range_cm){
+    while(rover::range() < go_range_cm){
+      rover::back_curve(0, rover::max_speed);
+      //rover::right(rover::max_speed);
+    }
+    delay(300);
+  }else{
+    rover::forward(rover::max_speed);
+  }
 }
+
