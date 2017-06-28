96af5c68eda2849727edc0e76f58eb33b0dff93c
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-06-28 17:57:10 +0100

    Switched to DEFINE_TEST macro

--- src/image/test/main.cpp
+++ src/image/test/main.cpp
@@ -9,41 +9,41 @@
 void set_up() {
 }
 
-void test_max_index(){
+DEFINE_TEST(test_max_index)
   int values[] = {4,2,3,6,1,5};
   assertEqual(3, max_index(6, values));
 }
 
-void test_max_index_last(){
+DEFINE_TEST(test_max_index_last)
   int values[] = {4,2,3,1,5,6};
   assertEqual(5, max_index(6, values));
 }
 
-void test_max_index_first(){
+DEFINE_TEST(test_max_index_first)
   int values[] = {6,4,2,3,1,5};
   assertEqual(0, max_index(6, values));
 }
-void test_max_index_duplicate(){
+DEFINE_TEST(test_max_index_duplicate)
   int values[] = {4,2,6,3,1,6,5};
   assertEqual(2, max_index(7, values));
 }
 
-void test_min_index(){
+DEFINE_TEST(test_min_index)
   int values[] = {4,2,3,1,5,6};
   assertEqual(3, min_index(6, values));
 }
 
-void test_min_index_last(){
+DEFINE_TEST(test_min_index_last)
   int values[] = {4,2,3,5,6,1};
   assertEqual(5, min_index(6, values));
 }
 
-void test_min_index_first(){
+DEFINE_TEST(test_min_index_first)
   int values[] = {1,4,2,3,5,6};
   assertEqual(0, min_index(6, values));
 }
 
-void test_min_index_duplicate(){
+DEFINE_TEST(test_min_index_duplicate)
   int values[] = {4,1,2,3,1,5,6};
   assertEqual(1, min_index(7, values));
 }
