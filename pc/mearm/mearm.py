bcdf71bdab3475ad8649075ad3f95b8c3b8b538c
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-08-09 14:02:57 +0100

    Added parsing of inbound point messages

--- pc/mearm/mearm.py
+++ pc/mearm/mearm.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+parsers={b'P': lambda l: (b'P',int(l[0]),int(l[1]),int(l[2]),l[3]==b'O')}
+
 class Arm:
   def __init__(self,ser):
     self.ser=ser
 
-  def move_to(self,x,y,z,is_open):
-    self.ser.write('M:{}:{}:{}:{}\n'.format(x,y,z,'O' if is_open else 'C').encode('utf-8'))
+  def move_to(self,pan,shoulder,elbow,is_open):
+    self.ser.write('M:{}:{}:{}:{}\n'.format(pan,shoulder,elbow,'O' if is_open else 'C').encode('utf-8'))
+
+  def read(self):
+    line=self.ser.readline().split(b':')
+    return parsers[line[0]](line[1:])
+
--- pc/mearm/test_mearm.py
+++ pc/mearm/test_mearm.py
@@ -5,10 +5,16 @@ from unittest import main,TestCase
 class MockSerial:
   def __init__(self):
     self.out_buffer=b""
+    self.in_buffer=b""
   
   def write(self,s):
     self.out_buffer+=s
     return len(s)
+
+  def readline(self):
+    [line, rest]=self.in_buffer.split(b'\n')
+    self.in_buffer=rest
+    return line;
 
 class TestMoveTo(TestCase):
   def setUp(self):
@@ -18,6 +24,15 @@ class TestMoveTo(TestCase):
   def test_single(self):
     self.arm.move_to(135,10,120,False)
     self.assertEqual(b'M:135:10:120:C\n',self.ser.out_buffer)
+
+class TestRecord(TestCase):
+  def setUp(self):
+    self.ser=MockSerial()
+    self.arm=mearm.Arm(self.ser)
+
+  def test_single(self):
+    self.ser.in_buffer=b'P:45:85:125:O\n'
+    self.assertEqual((b'P',45,85,125,True),  self.arm.read())
 
 if __name__=='__main__':
   main()
